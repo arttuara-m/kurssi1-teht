551edb8e7bb9902070e968269db0cbbeaa87b51a
arttukeuda <127646725+arttukeuda@users.noreply.github.com>
2025-01-17 10:19:03 +0200

    Moduulin kolme kaikki tehtävät (4) tehty.

--- Moduuli3/tehtava3.1.py
+++ Moduuli3/tehtava3.1.py
@@ -4,6 +4,3 @@ if kuhanpituus < 37:
     puute = str(37 - kuhanpituus)
     print("Kala on liian pieni, päästä se takaisin järveen.")
     print("Kuha on " + puute + " senttimetriä alle sallitun rajan.")
-
-
-
