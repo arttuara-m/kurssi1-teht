cb7a7c6129cd69ddad31b7c1a2f31763bf1d6ab5
arttukeuda <127646725+arttukeuda@users.noreply.github.com>
2025-03-28 10:41:44 +0200

    Moduulin 12 kaikki tehtävät (2) tehty

---
+++ Moduuli12/tehtava12.2.py
@@ -0,0 +1,10 @@
+import requests
+
+appid = "afd637bf7cd7aa962ceb18ffed800338"
+paikkakunta = input("Syötä haluttu paikkakunta suomeksi: ")
+pyynto = f"https://api.openweathermap.org/data/2.5/weather?q={paikkakunta}&appid={appid}&units=metric&lang=fi"
+vastaus = requests.get(pyynto).json()
+print(f'{vastaus["weather"][0]["description"]}')
+print(f'{vastaus["main"]["temp"]}C')
+
+#Jos haluat testata, laita oma API-avain placeholderin tilalle.
